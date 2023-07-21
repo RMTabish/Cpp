a7ee60a00ca352270b5fb76a4fb9d35ce416f352
RMTABISH <raimuhammadtabish@gmail.com>
2023-07-21 21:19:58 +0500

    Upload new code

---
+++ to be posted/Source.cpp
@@ -0,0 +1,30 @@
+//#include"Header1.h"
+#include<iostream>
+#include"Header2.h"
+using namespace std;
+int main()
+{
+	//cout << "enter first string";
+	//char oop[]={""};
+	//cin >> oop;
+	//char poo[]={ "" };
+	//cin >> poo;
+	//cout << poo<<endl;
+	//cout << oop<<endl;
+	//swap(oop, poo);
+	//cout << poo<<endl;
+	//cout << oop<<endl;
+	//delete oop;
+	//delete poo;
+
+	cout << "enter the string" << endl;
+	char hehe[]={""};
+	cin >> hehe;
+
+	cout << "enter the character";
+	char hoho;
+	cin >> hoho;
+	find(hehe, hoho);
+
+
+}
